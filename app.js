28571b7d5c1087dcd27bb54f1a2eb11fd9207e4d
Askari64 <95219928+Askari64@users.noreply.github.com>
2022-09-16 23:58:38 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,25 @@
+let screen = document.querySelector(".output");
+let buttons = document.querySelectorAll(".btn");
+let clear = document.querySelector(".clear");
+let equal = document.querySelector(".equal");
+
+buttons.forEach(function (button){
+    button.addEventListener('click', function(e) {
+        let value = e.target.dataset.number;
+        screen.value += value;
+    })
+})
+
+equal.addEventListener('click' , function(e){
+    if(screen.value === '') {
+        screen.value = "";
+    }else{ 
+        let answer = eval(screen.value);
+    screen.value = answer;
+    }
+})
+
+clear.addEventListener('click', function(e) {
+    screen.value = '';
+})
+
